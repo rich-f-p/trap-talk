f2f3c4af349fd17f04b24a37f2675851734e7196
rich p <richardferry3@gmail.com>
2022-06-07 14:45:32 -0700

    fix error with mongoose connection

--- main/server/config/connection.js
+++ main/server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI /* || 'mongodb://localhost/databaseNameHere' */, {
+mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost:27017/databaseNameHere', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
