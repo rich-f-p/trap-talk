510a26a34c058e7b4f73122bef590b9e10d035a2
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-06-07 22:37:50 -0700

    add API.js routes

--- main/client/src/utils/API.js
+++ main/client/src/utils/API.js
@@ -1,3 +1,12 @@
+export const getMe = (token) => {
+  return fetch('/api/users/me', {
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
+
 export const loginUser = (userData) => {
     return fetch('/api/users/login', {
       method: 'POST',
@@ -7,3 +16,36 @@ export const loginUser = (userData) => {
       body: JSON.stringify(userData),
     });
   };
+
+  export const createUser = (userData) => {
+    return fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+
+  export const sendMessage = (messageData, token) => {
+    return fetch('/api/users', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(messageData),
+    });
+  };
+
+  export const receiveMessage = (messageData, token) => {
+    return fetch('/api/users', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(messageData),
+    });
+  };
+  
