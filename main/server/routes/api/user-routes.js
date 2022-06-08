27393f52918866c9ff29f80ae823d4980cffea84
rich p <richardferry3@gmail.com>
2022-06-07 21:41:45 -0700

    create routes to get, post, and put

--- main/server/routes/api/user-routes.js
+++ main/server/routes/api/user-routes.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
-const {test} = require('../../controllers/user-controller');
-
+const {
+    test,
+    singleUser,
+    createUser,
+    login,
+    saveFriend,
+    addConvo,
+} = require('../../controllers/user-controller');
+const { authMiddleware} = require('../../utils/auth');
 //test http://localhost:3001/api/users/
-router.route('/').get(test);
+//router.route('/').get(test);
+
+router.route('/').post(createUser).put(authMiddleware,saveFriend);
+
+router.route('/login').post(login);
+
+router.route('/user').get(singleUser);
+
+router.route('/message').put(addConvo);
+
 
 module.exports = router;
