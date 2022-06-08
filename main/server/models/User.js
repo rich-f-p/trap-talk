78679f4f51531685c538cb88bf5e2041c40ca24e
rich p <richardferry3@gmail.com>
2022-06-07 18:28:05 -0700

    add bcrypt hash

--- main/server/models/User.js
+++ main/server/models/User.js
@@ -15,7 +15,6 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            match: [],
         },
         password:{
             type: String,
@@ -30,6 +29,15 @@ const userSchema = new Schema(
         },
     }
 );
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+  });
+
 
 
 
