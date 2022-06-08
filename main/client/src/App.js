06baa308402875a8ebc72bbd733a69b74a4f27d1
Maya Morales <mayainomorales@gmail.com>
2022-06-07 20:45:34 -0700

    fixing merge

--- main/client/src/App.js
+++ main/client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
+import Login from './pages/Login';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,6 +18,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Login/>
     </div>
   );
 }
