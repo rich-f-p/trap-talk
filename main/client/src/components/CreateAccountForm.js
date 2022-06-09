097c1059dc7df6c721ae53a7a1fc5421e221629a
Maya Morales <mayainomorales@gmail.com>
2022-06-08 21:49:20 -0700

    fixing create account form

--- main/client/src/components/CreateAccountForm.js
+++ main/client/src/components/CreateAccountForm.js
@@ -42,43 +42,49 @@ const CreateUserForm = () => {
   };
 
   return (
-        <div id="signup-form" className="w-full justify-center hidden mt-50">
-            <div className="card w-96 bg-base-100 shadow-xl">
+        <div id="login-form" className="w-full flex justify-center mt-50">
+            <div className="card w-96 bg-base-100 shadow-xl flex justify-center">
                 <div className="card-body">
-                    <h2 className="card-title">Sign-Up</h2>
+                    <h2 className="card-title">Create Account</h2>
                     <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Username</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="Type here"
-                        onChange={handleInputChange}
-                        value={userFormData.username}
-                        required
-                        className="input input-bordered w-full max-w-xs"
-                    />
+                        <label className="label">
+                            <span className="label-text">Username</span>
+                        </label>
+                        <input
+                            id="username-login"
+                            type="text"
+                            placeholder="Type here"
+                            className="input input-bordered w-full max-w-xs"
+                        />
                     </div>
                     <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Password</span>
-                    </label>
-                    <input
-                        type="password"
-                        placeholder="Type here"
-                        onChange={handleInputChange}
-                        value={userFormData.password}
-                        required
-                        className="input input-bordered w-full max-w-xs"/>
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input
+                            id="password-login"
+                            type="password"
+                            placeholder="Type here"
+                            className="input input-bordered w-full max-w-xs"
+                        />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Re-enter Password</span>
+                        </label>
+                        <input
+                            id="password-login"
+                            type="password"
+                            placeholder="Type here"
+                            className="input input-bordered w-full max-w-xs"
+                        />
                     </div>
                     <div className="card-actions">
-                        <button className="btn btn-primary" id="main-create-btn" onClick={() => handleFormSubmit()}
-                        disabled={!(userFormData.username && userFormData.password)}
-                        type='submit' 
-                        variant='success'
-                        >Create Account</button>
+                        <button className="btn btn-neutral w-full"
+                            id="main-create-btn">
+                        Create Account</button>
                         <div className="divider w-full">OR</div>
-                        <button id="login-btn" className="btn btn-secondary w-full" as={Link} to='/login'>Login</button>
+                        <button id="login-btn" className="btn btn-secondary w-full">Login</button>
                     </div>
                 </div>
             </div>
