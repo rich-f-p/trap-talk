bf19e5a78428e9f8b90beeed9b09e5ac61dee3ee
Maya Morales <mayainomorales@gmail.com>
2022-06-09 14:25:08 -0700

    fixing two navbar visibility on page

--- main/client/src/components/Navbar.js
+++ main/client/src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
                     </button>
                 </div>
                 <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl">Trap Talk</a>
                 </div>
                 <div className="flex-none">
                     <button className="btn btn-square btn-ghost">
