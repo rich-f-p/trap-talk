f75585cdd993c0e7cef921cf490ccf269f2fff27
Maya Morales <mayainomorales@gmail.com>
2022-06-10 00:02:58 -0700

    add my friends card for friends list

--- main/client/src/pages/Profile.js
+++ main/client/src/pages/Profile.js
@@ -13,13 +13,27 @@ const Profile = () => {
     return (
         <><Navbar />
 
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <div className="card-body">
+            <div className="card flex place-items-center w-96 bg-base-100 shadow-xl">
+                <div className="card-body flex place-content-center">
                     <h2 className="card-title">My Profile</h2>
                     <p></p>
                 </div>
                 <figure><img src={avatar} alt="Avatar" /></figure>
-            </div></>
+            </div>
+
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <figure class="px-10 pt-10">
+                    <img src='' alt="Friends" class="rounded-xl" />
+                </figure>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">My Friends</h2>
+                    <p></p>
+                    <div class="card-actions">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
 
     );
 };
