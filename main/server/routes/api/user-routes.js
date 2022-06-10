6661709eb931e7eb6b0d43d72f6db7e2115c5aa3
rich p <richardferry3@gmail.com>
2022-06-09 23:59:18 -0700

    add route to get logged in user's data

--- main/server/routes/api/user-routes.js
+++ main/server/routes/api/user-routes.js
@@ -18,7 +18,11 @@ router.route('/add/:username').put(/*authMiddleware,*/saveFriend);
 
 router.route('/login').post(login);
 
+//both of these get a single user 
 router.route('/single/:id').get(singleUser);
+//above gets a user by id
+router.route('/me').get(authMiddleware, singleUser);
+// gets a user through token
 
 router.route('/message/:user/:_id').put(addConvo);
 
